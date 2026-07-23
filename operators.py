ec9e94d041ebe1e94ab4ebcfdb1aaff86723488e
CHAVDA DHARMESH <chavdadharmesh2005@gmail.com>
2026-07-23 18:52:22 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,55 @@
+# Arithmetic Operators
+
+a = 10
+b = 20
+c = 10
+
+print("add is :",a+b)
+print("sub is :",a-b)
+print("mul is:",a*b)
+print("div is:",a/b)
+print("Modulus :",a%b)
+
+# Logical operators
+# > Relational operator
+
+if(a>5 and b>15):
+    print("True")
+else:
+    print("fales")
+
+if(a>20 or b>19):
+    print("True")
+else:
+    print("fales")
+
+print(not(a>5))
+
+#Relational Operator
+
+if(a==b):
+    print("true")
+else:
+    print("false")
+
+if(a != b):
+    print("not Equal")
+else:
+    print("equal")
+
+if (a<b):
+    print("a is less then b")
+else:
+    print("a is greter then b")
+
+if(a>=c):
+    print("a is grater then and equl to c")
+else:
+    print("a is not equal or greater")
+
+if(a<=b):
+    print("a is less then or equal to b")
+else:
+    print("not lesser or equal")
+
+     
