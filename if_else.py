b94e3726b9d7112e44680594183e2ed5b3d9794b
CHAVDA DHARMESH <chavdadharmesh2005@gmail.com>
2026-07-30 20:04:59 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,26 @@
+'''1. Write a program to demonstrate conditional
+statements using if if-else and if-elif-else.'''
+
+a = int(input('enter value for a : '))
+
+if a>0 :
+    print('The number is positive')
+
+
+
+if a%2 == 0:
+    print('the number is even')
+else:
+    print('number is odd')
+
+
+if a > 0:
+    print('number is positive')
+elif a < 0:
+    print('number is negative')
+else:
+    print('number is zero')
+
+
+
+    
