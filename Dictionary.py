6ca369869a01118768c6a317729d4558064edf56
CHAVDA DHARMESH <chavdadharmesh2005@gmail.com>
2026-07-30 19:55:31 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,16 @@
+''' Write a program to create a dictionary and
+demonstrate dictionary methods and iteration. '''
+
+Dictionary = {"name":"Dharmesh","age":21,"contect":"8200842477"}
+
+print(Dictionary)
+
+# access single element
+
+print(Dictionary["name"])
+
+#loop with dictionary
+
+for i in Dictionary:
+    print(Dictionary)
+
