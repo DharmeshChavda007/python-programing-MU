63ec8fd33f4f7a51f7edf208991c8ffd95bb86cb
CHAVDA DHARMESH <chavdadharmesh2005@gmail.com>
2026-07-30 20:27:16 +0530

    Add files via upload

---
+++ list_string_dictionary_loop.py
@@ -0,0 +1,23 @@
+'''Write a program to iterate over lists strings and
+dictionaries using loops. '''
+# iterate list
+lst = ['Dharmesh','Rohan','Rahul','Bharat']
+
+for i in lst:
+    print(i)
+
+print('========================================')
+
+# iterate string
+name = "Dharmesh"
+
+for t in name:
+    print(t)
+
+print('========================================')
+
+# iterate dictioanry
+
+d = {'name':'Dharmesh' ,'age':20, 'mobile':8200842477}
+for i in d:
+    print(d)
