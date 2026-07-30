6066a5c563d94d92503cfb67bfbedd9a8fa37935
CHAVDA DHARMESH <chavdadharmesh2005@gmail.com>
2026-07-30 19:50:47 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,20 @@
+#6. Write a program to illustrate the use of tuples
+#and sets with basic operations.
+
+# Create a tuple
+numbers = (10, 20, 30, 40, 50)
+
+print("Tuple:", numbers)
+
+# Accessing elements
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+
+# Slicing
+print("Elements from index 1 to 3:", numbers[1:4])
+
+# Length of tuple
+print("Length of tuple:", len(numbers))
+
+
+
