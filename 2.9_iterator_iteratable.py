678f00e58d5d6737acd957318b8d0bcf25de2759
CHAVDA DHARMESH <chavdadharmesh2005@gmail.com>
2026-08-11 12:16:35 +0530

    Add files via upload

---
+++ 2.9_iterator_iteratable.py
@@ -0,0 +1,11 @@
+'''
+9. Write a program to demonstrate iterators and
+iterables in Python.
+'''
+
+numbers = [10, 20, 30, 40, 50]
+
+print("Iterable:", numbers)
+
+for i in numbers:
+    print('iterator : ',i)
