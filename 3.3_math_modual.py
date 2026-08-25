27ab0a5b7d7c205933aa58a73caa65fdd7f0a905
CHAVDA DHARMESH <chavdadharmesh2005@gmail.com>
2026-08-25 12:02:36 +0530

    Add files via upload

---
+++ 3.3_math_modual.py
@@ -0,0 +1,9 @@
+#3. Write a program to perform mathematical operations using math module.
+a = float(input("Enter the value for a :"))
+b = int(input("Enter the value for b :"))
+
+print("minimum is :",min(a,b))
+print("maximum is :",max(a,b))
+print("absolute is :",abs(a))
+print("power is :",pow(a,b))
+
